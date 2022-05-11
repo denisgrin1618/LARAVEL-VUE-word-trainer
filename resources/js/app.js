b6c33fde56d06ab0d5484c83ff5e0b6db6945036
denis <d.kovalev@mobidev.biz>
2022-05-11 15:12:12 +0300

    front vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+
+import Vue from 'vue'
+import App from './App.vue'
+// import vuetify from './plugins/vuetify'
+import router from './router'
+import vuetify from './plugins/vuetify' // path to vuetify export
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
